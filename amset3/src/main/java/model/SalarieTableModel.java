7a778b7af85505064c99eea4073f6afeeab51384
GalaxyJay <borcedre@gmail.com>
2026-03-26 08:24:42 +0100

    création model salarié

--- amset3/src/main/java/model/SalarieTableModel.java
+++ amset3/src/main/java/model/SalarieTableModel.java
@@ -4,10 +4,40 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 /**
  *
  * @author c.bories
  */
+public class SalarieTableModel extends AbstractTableModel{
+    private final String[] nomColonnes = {"id", "Nom", "Prenom", "Fonction","DateNaissance","ServiceId"};
+    private List<Salarie> salaries = new ArrayList<Salarie>();
+    private SalarieDao salDao;
+    
+    public SalarieTableModel() throws Exception {
+        this.salDao = new SalarieDao();
+        this.refresh();
+    }
 
+    @Override
+    public int getRowCount() {
+        return this.salaries.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+      return this.nomColonnes.length;  
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return this.salaries.get(rowIndex).getId();
+    }
+
+    private void refresh() {
+        this.salDao.getAll();
+    }
+}
